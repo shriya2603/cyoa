5c64e25873c85ece8b79b4991662c12d73f25837
shriya2603 <sundriyalshriya@gmail.com>
2022-10-29 18:07:13 +0530

    Added custom paths and temp

--- students/shriya/cyoa/cmd/cyoaweb/main.go
+++ students/shriya/cyoa/cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/shriya/cyoa"
 )
@@ -28,10 +30,95 @@ func main() {
 		panic(err)
 	}
 
-	// If user need to pass it own template it can do by provide it in newHandler func
-	h := cyoa.NewHandler(story, nil)
+	storyTpl := template.Must(template.New("").Parse(storyTempl))
+	hc := cyoa.NewHandler(story, cyoa.WithTemplate(storyTpl), cyoa.WithPathFunc(pathFn))
+	mux := http.NewServeMux()
+	//Custom : This story handler is using a custom function and template
+	// Because we use /story/ (trailing slash) all web requests
+	// whose path has the /story/ prefix will be routed here.
+	mux.Handle("/story/", hc)
+	//Default:  This story handler is using the default functions and templates
+	// Because we use / (base path) all incoming requests not
+	// mapped elsewhere will be sent here.
+	mux.Handle("/", cyoa.NewHandler(story))
 	fmt.Println("Starting the server on port \n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 	// fmt.Println("%+v\n", story)
 
 }
+
+// Custom paths and template
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	path = path[len("/story/"):]
+	return path
+}
+
+var storyTempl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p> {{.}} Some para</p>
+      {{end}}
+      {{if .Options}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section> 
+    <style>
+    body {
+      font-family: helvetica, arial;
+    }
+    h1 {
+      text-align:center;
+      position:relative;
+    }
+    .page {
+      width: 80%;
+      max-width: 500px;
+      margin: auto;
+      margin-top: 40px;
+      margin-bottom: 40px;
+      padding: 80px;
+      background: #FFFCF6;
+      border: 1px solid #eee;
+      box-shadow: 0 10px 6px -6px #777;
+    }
+    ul {
+      border-top: 1px dotted #ccc;
+      padding: 10px 0 0 0;
+      -webkit-padding-start: 0;
+    }
+    li {
+      padding-top: 10px;
+    }
+    a,
+    a:visited {
+      text-decoration: none;
+      color: #6295b5;
+    }
+    a:active,
+    a:hover {
+      color: #7792a2;
+    }
+    p {
+      text-indent: 1em;
+    }
+  </style>  
+  </body>
+</html>`
--- students/shriya/cyoa/story.go
+++ students/shriya/cyoa/story.go
@@ -81,24 +81,45 @@ var defaultHanderTempl = `
   </body>
 </html>`
 
-func NewHandler(s Story, t *template.Template) http.Handler {
-	if t == nil {
-		t = tpl
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
 	}
-	return handler{s, t}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 	path = path[1:]
+	return path
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 	if chapter, ok := h.s[path]; ok {
 		err := tpl.Execute(w, chapter)
 		if err != nil {
